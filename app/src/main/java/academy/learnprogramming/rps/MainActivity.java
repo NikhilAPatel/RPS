c80d2624652ca80f7ac6e2d9711dd4a415822cc8
Nikhil Patel <12345n6@gmail.com>
2018-02-20 14:56:29 -0500

    Removed action bar
    turns out it was as easy as changin DarkActionBar to NoActionBar in the styles.xml file

--- app/src/main/java/academy/learnprogramming/rps/MainActivity.java
+++ app/src/main/java/academy/learnprogramming/rps/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -90,6 +91,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 
         //Remove notification bar
         //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
